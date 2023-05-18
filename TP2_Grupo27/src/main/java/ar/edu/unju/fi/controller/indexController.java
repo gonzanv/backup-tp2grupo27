8eaa3bc59e0a75e3450905a90543a384c3d3731a
gonzanv <glskglskgk@gmail.com>
2023-05-18 20:42:30 -0300

    Se corrigieron definiciones semanticas de thymeleaf

--- TP2_Grupo27/src/main/java/ar/edu/unju/fi/controller/indexController.java
+++ TP2_Grupo27/src/main/java/ar/edu/unju/fi/controller/indexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class indexController {
 
-	@GetMapping("/Index")
+	@GetMapping("/Inicio")
 	public String getindex() {
 		return "Index";
 	}
