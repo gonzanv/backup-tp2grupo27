34912373dfaab7b71f4798dfe012c0629a6cd341
gonzanv <glskglskgk@gmail.com>
2023-05-21 05:49:28 -0300

    Se agrego y modificó Archivos necesarios para editar lista de productos

---
+++ TP2_Grupo27/src/main/java/ar/edu/unju/fi/listas/ListaProductos.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.listas;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import ar.edu.unju.fi.model.Producto;
+
+public class ListaProductos {
+	private List<Producto> productos;
+	
+	public ListaProductos(){
+		productos = new ArrayList<Producto>();
+		productos.add(new Producto("Bolso Transportador Liso MB",1,2999.99,"Bolsos",30,"/images/bolso1.jpg"));
+		productos.add(new Producto("Corral Hexagonal",2,1799.99,"Corrales",20,"/images/corral.png"));
+		productos.add(new Producto("Bolso Portacan",3,2499.99,"Bolsos",0,"/images/can.png"));
+	}
+	
+	public List<Producto> getProductos(){
+		return productos;
+	}
+	
+	public void setProductos(List<Producto> productos) {
+		this.productos=productos;
+	}
+}
